091124668d414e505ae4cf1398b704ebf81ddebc
Eli Taylor <etaylor@kickbackpoints.com>
2014-02-24 13:07:36 -0700

    Append description to the recipe as well as syntax change for the bash resource

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,15 +1,17 @@
 #
 # Cookbook Name:: duo-security
+# Author:: Eli Taylor (Much DevOps)
+# Description:: Such secure. Wow.
 # Recipe:: default
 #
-# Copyright 2013, KickBack Points, LLC
+# Copyright 2013-2014, KickBack Points, LLC
 #
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe 'build-essential'
+include_recipe "build-essential"
 
-if platform?('ubuntu', 'debian')
+if platform?("ubuntu", "debian")
   # this recipe does not work on Ubuntu 9.04
   return if node.platform_version == '9.04'
 
@@ -18,8 +20,8 @@ if platform?('ubuntu', 'debian')
       action :install
     end
   end
-elsif platform?('redhat', 'centos', 'scientific', 'fedora', 'suse')
-  %w{openssl-devel pam-devel}.each do |pkg|
+elsif platform?("redhat", "centos", "scientific", "fedora", "suse", "arch", "freebsd")
+  %w{gcc make openssl-devel python-devel pam-devel}.each do |pkg|
     package pkg do
       action :install
     end
@@ -34,16 +36,14 @@ remote_file "#{Chef::Config[:file_cache_path]}/duo_unix-#{duo_unix_version}.tar.
   action :create_if_missing
 end
 
-bash 'compile-duo_unix' do
+bash "compile-duo_unix" do
   cwd Chef::Config[:file_cache_path]
   code <<-EOH
     tar zxvf duo_unix-#{duo_unix_version}.tar.gz
     cd duo_unix-#{duo_unix_version}
-    ./configure --prefix=/usr
-    make -s
-    make install
+    ./configure --prefix=/usr && make && make install
   EOH
-  creates node['duosecurity']['config_dir']
+  creates "#{node['duosecurity']['config_dir']}/login_duo.conf"
 end
 
 template "#{node['duosecurity']['config_dir']}/login_duo.conf" do
